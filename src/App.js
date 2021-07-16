d3300cd7a2c44eee831df5d703be60f78101c44a
Ntwari Moise <mozayntwali@gmail.com>
2021-07-16 19:18:25 +0200

    Logic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import { useState } from "react";
 import SearchBox from "./components/searchBox";
 
+const apiUrl = 'https://jsonplaceholder.typicode.com/albums';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleChange = (query) => {
+    setSearchQuery(query);
+  }
+
+  const handleSearch = async () => {
+    console.log(searchQuery);
+    const data = await (await fetch(`${apiUrl}/${searchQuery}/photos`)).json()
+    
+    setPosts(data);
+    setSearchQuery('')
+  }
 
   return (
     <div className="container">
       <div className="input-group mt-5">
-        <SearchBox />
+        <SearchBox value={searchQuery} onChange={handleChange} />
         <div className="input-group-append">
-          <button className="input-group-text border-0"><small>Get Album Photos By Id</small></button>
+          <button className="input-group-text border-0" onClick={handleSearch}><small>Get Album Photos By Id</small></button>
         </div>
       </div>
       <div className="row">
-        <div className="col-lg-4 my-2">
-            <div className="card border-0 shadow-sm">
-              <div className="card-body">
-                <div className="media align-items-center">
-                  <div className="img-box">
-                    <img className="img-fluid" alt="" />
-                  </div>
-                  <div className="media-body ml-2">
-                    <h3><small className="text-black-50">title</small></h3>
+        {posts.length !== 0 ? 
+          posts?.map((post, key) => (
+            <div key={key} className="col-lg-4 my-2">
+              <div className="card border-0 shadow-sm">
+                <div className="card-body">
+                  <div className="media align-items-center">
+                    <div className="img-box">
+                      <img src={post.thumbnailUrl} className="img-fluid" alt="" />
+                    </div>
+                    <div className="media-body ml-2">
+                      <h3><small className="text-black-50">{post.title}</small></h3>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+          ))
+        : (
+          <div className="d-flex justify-content-center">
+            <h2 className="font-bold text-black-20">No Post Yet!!</h2>
           </div>
-        <div className="d-flex justify-content-center">
-          <h2 className="font-bold text-black-20">No Post Yet!!</h2>
-        </div>
+        )}
       </div>
     </div>
   );
